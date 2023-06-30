69fa2b643e612c4393b18bdafcf0190adefdc3a1
Levon <dr.meroy@yandex.ru>
2023-06-30 03:07:05 +0300

    Added Task02, Task04, Task 06, Task 08

---
+++ Task04/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 4: Напишите программу, которая принимает на
+// вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int numberC = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB && numberA > numberC)
+{
+    Console.WriteLine($"Максимальное число: {numberA}");
+}
+else if (numberB > numberA && numberB > numberC)
+{
+    Console.WriteLine($"Максимальное число: {numberB}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число: {numberC}");
+}
---
+++ Task08/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 8: Напишите программу, которая на вход
+// принимает число (N), а на выходе показывает все чётные
+// числа от 1 до N.
+
+Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+if (number <= 0)
+{
+    Console.WriteLine("Ошибка ввода. Введите целое положительное число");
+}
+while (count <= number)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write($"{count} ");
+    }
+    count++;
+}
