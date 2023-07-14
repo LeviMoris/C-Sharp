72e41b21f4c9bc8004841ba0c8f00ca66575a213
Levon <dr.meroy@yandex.ru>
2023-07-15 00:28:10 +0300

    Added Tasks 31, 32, 33, 35, 37

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -12,17 +12,17 @@ PrintArray(array);
 
 void FillArray(int[] arr)
 {
-Random rnd = new Random();
-for (int i = 0; i < arr.Length; i++)
-{
-arr[i] = rnd.Next(2);
-}
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(2);
+    }
 }
 
 void PrintArray(int[] arr)
 {
-for (int i = 0; i < arr.Length; i++)
-{
-Console.Write($"{arr[i]} ");
-}
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
 }
