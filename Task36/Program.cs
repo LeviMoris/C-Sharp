fc056b69998658f43122c3afec1e92e22dd7fba5
Levon <dr.meroy@yandex.ru>
2023-07-15 02:27:48 +0300

    Added Tasks 34, 36, 38

---
+++ Task36/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr, string sep = ", ")
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep}");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+int GetSumNumbersOddIndex(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0) sum += arr[i];
+    }
+    return sum;
+}
+
+int[] array = CreateArrayRndInt(4, 0, 9);
+Console.Write("[");
+PrintArray(array);
+Console.Write("]");
+Console.WriteLine();
+
+int sumNumbersOddIndex = GetSumNumbersOddIndex(array);
+Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях = {sumNumbersOddIndex}");
---
+++ Task38/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+double[] CreateArrayRndDouble(int size, int min, int max)
+{
+    double[] arr = new double[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.NextDouble() * (max - min) + min;
+    }
+    return arr;
+}
+
+void PrintArrayDouble(double[] arr, string sep = "; ", int round = 1)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        double num = Math.Round(arr[i], round);
+        if (i < arr.Length - 1) Console.Write($"{num}{sep}");
+        else Console.Write($"{num}");
+    }
+}
+
+double GetMaxNumber(double[] arr)
+{
+    double maxNumber = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > maxNumber) maxNumber = arr[i];
+    }
+    return maxNumber;
+}
+
+double GetMinNumber(double[] arr)
+{
+    double minNumber = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < minNumber) minNumber = arr[i];
+    }
+    return minNumber;
+}
+
+double GetDifferenceMinMaxNumbers(double max, double min)
+{
+    double result = max - min;
+    return result;
+}
+
+
+double[] array = CreateArrayRndDouble(5, 0, 20);
+Console.Write("[");
+PrintArrayDouble(array);
+Console.Write("]");
+Console.WriteLine();
+
+double maxNumber = GetMaxNumber(array);
+double minNumber = GetMinNumber(array);
+
+double differenceMinMaxNumbers = Math.Round(GetDifferenceMinMaxNumbers(maxNumber, minNumber), 1);
+Console.WriteLine($"Разница между максимальным и минимальным элементом массива = {differenceMinMaxNumbers}");
