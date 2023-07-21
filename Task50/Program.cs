e0fe22e40e78119cfa51b0d3f31301e32713701c
Levon <dr.meroy@yandex.ru>
2023-07-21 03:25:15 +0300

    Added Tasks 47, 50, 52

---
+++ Task50/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции
+// элемента в двумерном массиве, и возвращает значение
+// этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого элемента в массиве нет
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    // 0 1
+    int[,] matrix = new int[rows, columns]; // 3 x 4
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // matrix.GetLength(0) = 3
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // matrix.GetLength(1) = 4
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++) // matrix.GetLength(0) = 3
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++) // matrix.GetLength(1) = 4
+        {
+            Console.Write($"{matrix[i, j], 5} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4, -100, 100);
+PrintMatrix(array2d);
+
+Console.WriteLine("Введите индекс строки (начиная с 0)");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите индекс столбца (начиная с 0)");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x >= array2d.GetLength(0) && y >= array2d.GetLength(1) && x < 0 && y < 0)
+{
+    Console.WriteLine("такой координаты в массиве нет");
+    return;
+}
+Console.WriteLine($"Элемент массива с такими координатами: {array2d[x, y]}");
