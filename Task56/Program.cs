1e50e9c8542f581dc271e0a5e15bf91919f7536b
Levon <dr.meroy@yandex.ru>
2023-08-02 04:26:59 +0300

    Added Tasks 54, 56, 58, 60, 62

---
+++ Task56/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив.
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и
+// выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    // 0 1
+    int[,] matrix = new int[rows, columns]; // 3 x 4
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // matrix.GetLength(0) = 3
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // matrix.GetLength(1) = 4
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++) // matrix.GetLength(0) = 3
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++) // matrix.GetLength(1) = 4
+        {
+            Console.Write($"{matrix[i, j], 5} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int MatrixSumRowsElem(int[,] matrix)
+{
+    int temp = 0;
+    int res = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+        if (i == 0)
+            temp = sum;
+        else if (sum < temp)
+        {
+            temp = sum;
+            res = i;
+        }
+    }
+    return res;
+}
+
+int[,] array2d = CreateMatrixRndInt(4, 3, 1, 9);
+PrintMatrix(array2d);
+int result = MatrixSumRowsElem(array2d);
+Console.WriteLine($"Индекс строки с наименьшей суммой элементов: {result}");
---
+++ Task60/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+// Напишите программу, которая будет построчно выводить массив,
+// добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// Результат:
+// 66(0,0,0) 25(0,1,0) 27(0,0,1) 90(0,1,1)
+// 34(1,0,0) 41(1,1,0) 26(1,0,1) 55(1,1,1)
+
+
+int[,,] CreateMatrix3DRndInt(int rows, int columns, int deep, int min = -10, int max = 10)
+{
+    //                         0     1      2
+    int[,,] matrix = new int[rows, columns, deep];
+
+    Random rnd = new Random();
+    for (int x = 0; x < matrix.GetLength(0); x++)
+    {
+        for (int y = 0; y < matrix.GetLength(1); y++)
+        {
+            for (int z = 0; z < matrix.GetLength(2); z++)
+            {
+                matrix[x, y, z] = rnd.Next(min, max + 1);
+            }
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix3D(int[,,] matrix)
+{
+    for (int z = 0; z < matrix.GetLength(2); z++)
+    {
+        Console.WriteLine();
+        for (int x = 0; x < matrix.GetLength(0); x++)
+        {
+            Console.Write("|");
+            for (int y = 0; y < matrix.GetLength(1); y++)
+            {
+                Console.Write($"{matrix[x, y, z], 5}({x},{y},{z}) ");
+            }
+            Console.WriteLine("|");
+        }
+    }
+}
+
+int[,,] array3d = CreateMatrix3DRndInt(4, 3, 3, 1, 9);
+PrintMatrix3D(array3d);
